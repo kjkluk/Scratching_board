e878073741c3f75b154991582e3a90dd915f4dd4
出雲 <95124022+kjkluk@users.noreply.github.com>
2025-12-31 00:52:29 +0800

    Add comments for clarification in rebar_and_spam_and_reason.pl
    Added comments in Chinese explaining the context of the code.

--- rebar_and_spam_and_reason.pl
+++ rebar_and_spam_and_reason.pl
@@ -17,3 +17,7 @@ else{
 @hard_working_people = qw/aunt1 aunt2 aunt3 aunt4 uncle1 uncle2 mother/;
 my $count_daoshi_item = 1;
 my $count_unknown_species =1;
+
+# 钢筋，钢筋，火腿罐头和回炉重造之理由。
+# 彼等为一难以改造之物。若尔辈不能究其之理，则尔等之力毫无意义。此类下等蝻之写真。其实际状态将忘日之所至之人之态昭然若揭，然则有一尸悬于其上。若无等不知其能否继续，然则此处是否有关的确有关。七人努力工作，然则不知其果。崆峒道士有一物。吾辈皆尽力于此，除却一不可名状之物种耳。
+
